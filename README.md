f426c6a9b531e16a8df5341d07b88c187c7b7f1c
Yuvraj Tiwari <raaztiwari373@gmail.com>
2024-01-20 00:34:58 +0530

    nothing change

---
+++ README.md
@@ -0,0 +1,5 @@
+# Digital World Clock
+ Here i make a digitak World clock 
+ # mini Project
+    this is a mini project and i make to learn to javascript
+    
--- clock.js
+++ clock.js
@@ -15,12 +15,3 @@ second.innerHTML = (d.getSeconds()<10?'0':'')+d.getSeconds();
 
 
 },1000);
-
-if(hour == 0){
-    hour = 12;
-}
-
-if(hour > 12){
-    hour = hour - 12;
-    session = "PM";
-}
