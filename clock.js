5260a7f02ec7ccdaba92ed6950d026d648d3bdc9
Yuvraj Tiwari <raaztiwari373@gmail.com>
2024-01-19 20:28:54 +0530

    mini digital clock project

---
+++ clock.js
@@ -0,0 +1,26 @@
+const hour = document.querySelector('#hours');
+const minute = document.querySelector('#minute');
+const second = document.querySelector('#second');
+const timeFormat =  document.querySelector('#timeFormat');
+
+
+setInterval(()=>{
+let d = new Date();
+
+
+
+hour.innerHTML = (d.getHours()<10?'0':'')+d.getHours();
+minute.innerHTML = (d.getMinutes()<10?'0':'')+d.getMinutes();
+second.innerHTML = (d.getSeconds()<10?'0':'')+d.getSeconds();
+
+
+},1000);
+
+if(hour == 0){
+    hour = 12;
+}
+
+if(hour > 12){
+    hour = hour - 12;
+    session = "PM";
+}
